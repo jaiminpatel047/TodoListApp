2e3e2ec66ea2fc36c94b4a145dfb86f3fa52c796
JAIMIN PATEL <124573877+jaiminpatel047@users.noreply.github.com>
2023-04-05 19:32:10 +0530

    7/32

--- app.js
+++ app.js
@@ -6,13 +6,15 @@ let listContainer = document.querySelector(".todo-list");
 let toastBox = document.querySelector(".toster");
 let formCss = document.querySelector('.form');
 let task = document.querySelectorAll('.todo-list-item');
+let taskIconButton = document.querySelector('.taskCound');
+let overlayTaskApper = document.querySelector('.taskEditeOverlay')
 
 // function
 button.addEventListener("click", () => {
   let todoValue = document.querySelector(".todo-input").value;
+
   if (todoValue !== "") {
     todoTask();
-
     toastBox.style.display = 'none';
     formCss.style.backgroundColor = 'rgb(181, 126, 54)';
   } else {
@@ -24,8 +26,13 @@ button.addEventListener("click", () => {
         toastBox.style.display = 'none';
     }, 3000);
   }
+
 });
 
+
+const taskStoreArray = [];
+
+// TODO TASK
 function todoTask() {
   let todoList = document.createElement("div");
   todoList.classList.add("todo-list-item");
@@ -33,7 +40,7 @@ function todoTask() {
   let todoValue = document.querySelector(".todo-input").value;
   todoList.innerHTML = `
             <div class="todo-list-value">
-                ${todoValue}
+              ${todoValue}
             </div>
               <button class="todo-item-update"><i class="fa-solid fa-pen-to-square"></i></button>
               <button class="todo-item-delete"><i class="fa-sharp fa-solid fa-trash"></i></button>
@@ -41,19 +48,67 @@ function todoTask() {
 
   listContainer.append(todoList);
   document.querySelector(".todo-input").value = "";
-  
 
   const deleteButton = todoList.querySelector('.todo-item-delete');
   const updateButton = todoList.querySelector('.todo-item-update');
   deleteButton.addEventListener('click', deleteTodo);
-  updateButton.addEventListener('click', updateTodo)
+  updateButton.addEventListener('click', updateTodo);
+
+
+  taskStoreArray.push(todoList);
+  document.querySelector('.totalTaskNumber').innerText = taskStoreArray.length;
+  console.log(taskStoreArray.length + " : " + taskStoreArray);
 }
 
+
+// delete 
 function deleteTodo(){
+  let showTask = document.querySelector('.totalTaskNumber');
+
     const todoItem = this.parentNode;
     todoItem.remove();
+  
+     taskStoreArray.pop(todoItem)
+     // if(taskStoreArray.length === 0){
+     //    showTask.innerText = null;
+     // }else{
+     //    showTask.innerText = taskStoreArray.length;
+     // }
+
+     // ES6 if condition 
+     taskStoreArray.length === 0?showTask.innerText = null:showTask.innerText = taskStoreArray.length;
+     
 }
 
+// update
 function updateTodo(){
-    console.log("update");
+   let taskOverlay = document.querySelector('.overlay');
+       taskOverlay.style.display = 'block'
+
+   let overlayClose = taskOverlay.
+   overlayClose.onClick = () => { taskOverlay.style.display = 'none' }
 }
+
+// Task Icon Show
+taskIconButton.onclick = () =>{
+  let taskIcon = document.querySelector('.taskShowIcon');
+  let totalTask = document.querySelector('.taskDisplay');
+
+  taskIconButton.classList.toggle('active');
+
+  // if(taskIconButton.classList.contains('active')){
+  //     taskIcon.style.display = 'none';
+  //     totalTask.style.display = 'block';
+  // }else{
+  //     taskIcon.style.display = 'block';
+  //     totalTask.style.display = 'none';
+  // }
+
+  // ES6 if condition
+  taskIconButton.classList.contains('active')
+  ? (taskIcon.style.display = 'none', totalTask.style.display = 'block')
+  : (taskIcon.style.display = 'block', totalTask.style.display = 'none');
+}
+
+// Overlay
+
